4bd53e64e920143cde7613266fee982bae82cc50
changraekang <storkcr@gmail.com>
2021-09-14 15:00:25 +0900

    Web화면 구성완료

---
+++ movie_model.py
@@ -0,0 +1,5 @@
+class MovieModel:
+    def __init__(self, title, rating, small_cover_image) :
+        self.title  = title
+        self.rating = rating
+        self.small_cover_image   = small_cover_image
